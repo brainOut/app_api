6b53fe22be2e52ed1f606d39426eec57a9420996
matthieu <mlamamra@jehann.fr>
2021-03-11 13:53:48 +0100

    code matthieu: env -> .env

---
+++ matthieu/app.py
@@ -0,0 +1,99 @@
+from flask import Flask, request, jsonify, make_response
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+import os
+from hashlib import sha256
+from datetime import datetime, timedelta
+import jwt
+from functools import wraps
+
+# charger le .env
+load_dotenv()
+
+# créer le serveur d'api
+app = Flask(__name__)
+
+# config db
+app.config['SECRET_KEY'] = os.environ.get('FLASK_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = f"postgresql://{os.environ.get('POSTGRES_USER')}:{os.environ.get('POSTGRES_PASS')}@{os.environ.get('POSTGRES_HOST')}/{os.environ.get('POSTGRES_DB')}"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+# db object
+db = SQLAlchemy(app)
+
+
+# model user
+class User(db.Model):
+
+    __tablename__ = 'auth_user'
+
+    id = db.Column('id', db.Integer, primary_key=True)
+    username = db.Column('username', db.String(100))
+    password = db.Column('password', db.String(255))
+
+
+# model project
+class Project(db.Model):
+
+    __tablename__ = 'app_project'
+
+    id = db.Column('id', db.Integer, primary_key=True)
+    name = db.Column('name', db.String(100))
+    token = db.Column('token', db.String(300))
+    url = db.Column('url', db.String(300))
+
+
+# authentication
+@app.route('/auth', methods=['GET'])
+def auth():
+
+    creds = request.authorization
+
+    if not creds or not creds.username or not creds.password:
+        return make_response('could not verify', 401, {'WWW.Authentication': 'Basic realm: "login required"'})
+
+    try:
+        username = User.query.filter_by(username=creds.username).first().username
+        project_token = Project.query.filter_by(token=sha256(creds.password.encode()).hexdigest())
+    except:
+        return make_response('could not verify', 401, {'WWW.Authentication': 'Basic realm: "login required"'})
+    else:
+        token = jwt.encode({
+            'username': username,
+            'exp': datetime.utcnow() + timedelta(minutes=30)
+        }, app.config['SECRET_KEY'])
+        return jsonify({'token': token})
+
+
+# décorateur de vérification du token
+def token_required(f):
+    @wraps(f)
+    def decorator(*args, **kwargs):
+        token = None if 'x-access-tokens' not in request.headers else request.headers['x-access-tokens']
+
+        if not token:
+            return jsonify({'message': 'a valid token is missing'})
+
+        try:
+            data = jwt.decode(token, app.config["SECRET_KEY"], options={"verify_signature": False})
+            user = User.query.filter_by(username=data['username']).first()
+        except:
+            return jsonify({'message': "token is invalid"})
+        else:
+            return f(user, *args, **kwargs)
+    return decorator
+
+
+@app.route('/project/<project_token>', methods=['GET'])
+@token_required
+def get_project( _, project_token):
+    project = Project.query.filter_by(token=project_token).first()
+    return jsonify({
+        "name": project.name,
+        "url": project.url
+    })
+
+
+# run app
+if __name__ == "__main__":
+    app.run("localhost", 8000, debug=True)
---
+++ matthieu/client.py
@@ -0,0 +1,65 @@
+import requests
+import sys
+import getopt
+
+
+class PentesterApiClient():
+
+    def __init__(self, url):
+        self.__url = url
+        self.__token = None
+        self.__project = None
+        self.error = None
+
+    def __auth(self, username, passwd):
+        try:
+            r = requests.get(f"{self.__url}/auth", auth=requests.auth.HTTPBasicAuth(
+                username, passwd))
+        except (requests.HTTPError, requests.RequestException) as e:
+            self.error = e
+        if r.status_code == 200:
+            self.__token = r.json()["token"]
+
+    def __get_project(self, project_token):
+        try:
+            r = requests.get(
+                f"{self.__url}/project/{project_token}",
+                headers={
+                    "Content-Type": "application/json",
+                    "x-access-tokens": self.__token
+                })
+        except (requests.HTTPError, requests.RequestException) as e:
+            self.error = e
+        if r.status_code == 200:
+            self.__project = r.json()
+
+    def __get_tests(self, project_token):
+        try:
+            r = requests.get(
+                f"{self.__url}/tests/{project_token}",
+                headers={
+                    "Content-Type": "application/json",
+                    "x-access-tokens": self.__token
+                })
+        except (requests.HTTPError, requests.RequestException) as e:
+            self.error = e
+        if r.status_code == 200:
+            self.__tests = r.json()
+
+    def launch(self, usr, pwd, tk):
+        self.__auth(usr, pwd)
+        if not self.error:
+            self.__get_project(tk)
+        return self.__project
+
+
+if __name__ == "__main__":
+    try:
+        optlist, args = getopt.getopt(sys.argv[1:], None, ["url=", "user=", "passwd=", "token="])
+        url, user, passwd, token = list(dict(optlist).values())
+    except (getopt.GetoptError, ValueError, OSError) as e:
+        sys.exit(e)
+    api = PentesterApiClient(url)
+    data = api.launch(user, passwd, token)
+    print(data)
+
